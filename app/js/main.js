47d148b6af240f1bd862bad8ac42c27611859aeb
Григорий <fastmotion1111@gmail.com>
2020-07-17 22:42:39 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    
+    $('').show().slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        dots:true,
+        cssEase: 'linear'
+    });
+
+});
+
+
